cc04d6dff60e4c898061aa26aa6153c0c4a8e3e8
chenxu <chenxu@shanhai.team>
2023-04-23 14:26:20 +0800

    Initial commit

---
+++ src/main/java/com/example/multithreadingprogramming/MultithreadingProgrammingApplication.java
@@ -0,0 +1,13 @@
+package com.example.multithreadingprogramming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultithreadingProgrammingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultithreadingProgrammingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/multithreadingprogramming/MultithreadingProgrammingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.multithreadingprogramming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultithreadingProgrammingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
