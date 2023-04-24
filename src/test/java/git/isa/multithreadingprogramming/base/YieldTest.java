8cf0ac2a02f3c2edb456df183c0675baecf838c8
chenxu <chenxu@shanhai.team>
2023-04-24 15:11:40 +0800

    test: yield

---
+++ src/test/java/git/isa/multithreadingprogramming/base/YieldTest.java
@@ -0,0 +1,32 @@
+package git.isa.multithreadingprogramming.base;
+
+/**
+ * @author cx
+ * @version 1.0
+ * @description:
+ * @date 2023/4/24 2:44 PM
+ */
+public class YieldTest implements Runnable {
+
+    YieldTest () {
+        Thread t = new Thread(this);
+        t.start();
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 5; i++) {
+            if ((i % 5) == 0) {
+                System.out.println(Thread.currentThread() + "yield cpu...");
+                 Thread.yield();
+            }
+        }
+        System.out.println(Thread.currentThread() + "is over");
+    }
+
+    public static void main(String[] args) {
+        new YieldTest();
+        new YieldTest();
+        new YieldTest();
+    }
+}
